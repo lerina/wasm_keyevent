49fc0fa59a6f9cc3b7904045f45f2a4c7a457aad
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2024-02-29 18:39:08 -0500

    setup js html, starting fn, display player in browser console

---
+++ notes.md
@@ -0,0 +1,10 @@
+## 18h15
+
+We have a basic model. A Player entity that has a position (x,y) and width and height.
+
+### First let's create a player and show its position
+
+- setup html, js. 
+- Starting function from rs
+- create player and display pos in console
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console::log_1;
+
 mod entity;
 
 use entity::Player;
 
+#[wasm_bindgen(start)]
+fn main() -> Result<(), JsValue> {
+    let player = Player::new(10.0, 20.0);
+    let (x, y) = player.get_pos();
+    log_1(&format!("player pos: {},{}", x, y).into());
+    Ok(())
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -13,4 +25,3 @@ mod tests {
         assert_eq!((10.0, 20.0), p.get_pos());
     }
 }
-
