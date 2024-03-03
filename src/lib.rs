06eeabc05df045c469d93f18e517582ab1573144
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2024-03-03 13:19:09 -0500

    key code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+use std::collections::HashMap;
+use std::cell::RefCell;
+use std::rc::Rc;
+//use lazy_static::lazy_static;
+//use std::sync::Mutex;
+//lazy_static! {
+//    static ref MUTABLE_GLOBAL_VARIABLE: Mutex<String> = Mutex::new("_".to_string());
+//}
+
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
@@ -5,7 +14,8 @@ use web_sys::console;
 const GAME_WIDTH: f64 = 600.0;
 const GAME_HEIGHT: f64 = 600.0;
 const PLAYER_INITIAL_SPEED: f64 = 1.0;
- 
+
+
 #[wasm_bindgen]
 #[derive(Clone, Copy)]
 pub struct Player {
@@ -17,18 +27,25 @@ pub struct Player {
 }
 
 #[wasm_bindgen]
+#[derive(Clone, Copy)]
 pub struct Game {
     pub width: f64,
     pub height: f64,
     pub player: Player,
+    pub keyevent: Option<i32>,
 }
-
 
 #[wasm_bindgen]
 impl Player {
     #[wasm_bindgen(constructor)]
     pub fn new(x: f64, y: f64, width: f64, height: f64, speed: f64) -> Player {
-        Player { x, y, width, height, speed }
+        Player {
+            x,
+            y,
+            width,
+            height,
+            speed,
+        }
     }
 
     pub fn draw(&self, ctx: &web_sys::CanvasRenderingContext2d) {
@@ -36,13 +53,14 @@ impl Player {
         ctx.stroke();
         ctx.fill();
     }
-    pub fn update(&mut self, keyevent: &str) {
+    pub fn update(&mut self, keyevent: i32) {
         let _ = match keyevent {
-            "Up" => self.y = (self.y - self.speed).max(self.height),
-            "Right" => self.x = (self.x + self.speed).min(GAME_WIDTH),
-            "Down" => self.y = (self.y + self.speed).min(GAME_HEIGHT - 100.0),
-            "Left" => self.x = (self.x - self.speed).max(self.width),
-            _ => (),
+            0 => self.y = (self.y - self.speed).max(self.height), //"ArrowUp"
+            1 => self.x = (self.x + self.speed).min(GAME_WIDTH), //"ArrowRight"
+            2 => self.y = (self.y + self.speed).min(GAME_HEIGHT - 100.0), //"ArrowDown"
+            3 => self.x = (self.x - self.speed).max(self.width), //"ArrowLeft"
+            4 => todo!(), //spacebar to shoot //" " 
+            _ => (), // do nothing 
         };
     }
 }
@@ -51,14 +69,26 @@ impl Player {
 impl Game {
     #[wasm_bindgen(constructor)]
     pub fn new(width: f64, height: f64, player: Player) -> Game {
-        Game { width, height, player }
+        Game { width, height, player, keyevent: None }
+    }
+    pub fn get_keyevent(&self) -> Option<i32> {
+        self.keyevent
+    }
+    pub fn set_keyevent(&mut self, keyevent: i32) {
+        self.keyevent = Some(keyevent);
+    }
+    pub fn update(&mut self) {
+        let _ = match self.keyevent {
+            Some(x) => self.player.update(x),
+            _ => {},
+        };
     }
 }
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
-   //
+    //
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
     let canvas = document
@@ -77,41 +107,76 @@ pub fn main_js() -> Result<(), JsValue> {
 
     set_common_style(&ctx);
     //
-    
-    let mut game = Game::new( GAME_WIDTH, GAME_HEIGHT, 
-                              Player::new((GAME_WIDTH - 25.0)/2.0, GAME_HEIGHT-100.0, 50.0, 45.0, PLAYER_INITIAL_SPEED));
+
+    let game = Rc::new( RefCell::new( Game::new(
+                            GAME_WIDTH,
+                            GAME_HEIGHT,
+                            Player::new(
+                                (GAME_WIDTH - 25.0) / 2.0,
+                                GAME_HEIGHT - 100.0,
+                                50.0,
+                                45.0,
+                                PLAYER_INITIAL_SPEED,
+                            ),
+                        ))
+    );
 
     wasm_bindgen_futures::spawn_local(async move {
-        //console::log_1(&"Draw Rect".into());
-        //let mut frame = -1.0;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            //input: simulation
-            //frame = (frame + 1.0) % 10.0;
-            //let frame_name = format!("Frame #{}", frame + 1.0);
+        let game = game.clone();
+        prepare_input(&canvas, *game.borrow()); 
 
+        let interval_callback = Closure::wrap(Box::new({
+            let game = game.clone();            
+            move || {
+            // input
+            let user_action = game.borrow().get_keyevent(); 
             // update:
-            game.player.update("Up"); 
+            game.borrow_mut().update();
             // draw
-            ctx.clear_rect(0.0, 0.0, game.width, game.height); //ctx.clear_rect(0.0, 0.0, 600.0, 600.0);
-            game.player.draw(&ctx); 
+            ctx.clear_rect(0.0, 0.0, game.borrow().width, game.borrow().height); //ctx.clear_rect(0.0, 0.0, 600.0, 600.0);
+            game.borrow().player.draw(&ctx);
             //console::log_1(&frame_name.into());
+            }
         }) as Box<dyn FnMut()>);
 
-        window.set_interval_with_callback_and_timeout_and_arguments_0(
-            interval_callback.as_ref().unchecked_ref(),
-            50,
-        );
+        let _ = window
+            .set_interval_with_callback_and_timeout_and_arguments_0(interval_callback
+                                                                        .as_ref()
+                                                                        .unchecked_ref(), 
+                                                                        500);
 
         interval_callback.forget();
-    });//^-- spawn_local
+    }); //^-- spawn_local
 
     Ok(())
-}//^--fn main_js
+} //^--fn main_js
 
+fn prepare_input(canvas: &web_sys::HtmlCanvasElement, game: Game) {
+    let mut game = game.clone();
+    let gamekeys: HashMap<String, i32> = HashMap::from([ ("ArrowUp".to_string(), 0), ("ArrowRight".to_string(),1) ,("ArrowDown".to_string(), 2),("ArrowLeft".to_string(), 3),(" ".to_string(), 4)]);  
+ 
+    let onkeydown = Closure::wrap(Box::new(move |keycode: web_sys::KeyboardEvent| {
+        console::log_1(&keycode.key().into());
+        game.set_keyevent(
+            *gamekeys.get(&keycode.key()).unwrap_or(&5)); //5 is do nothing
+        //game.get_keyevent().unwrap()        
+        console::log_1(&format!("key {}", game.get_keyevent().unwrap()).into());
+    }) as Box<dyn FnMut(web_sys::KeyboardEvent)>);
+
+    let onkeyup =
+        Closure::wrap(Box::new(move |keycode: web_sys::KeyboardEvent| {}) as Box<dyn FnMut(web_sys::KeyboardEvent)>);
+
+    canvas.set_onkeydown(Some(onkeydown.as_ref().unchecked_ref()));
+
+    canvas.set_onkeyup(Some(onkeyup.as_ref().unchecked_ref()));
+
+    onkeydown.forget();
+    onkeyup.forget();
+}
 
 fn set_common_style(ctx: &web_sys::CanvasRenderingContext2d) {
-  ctx.set_shadow_color("#d53");
-  ctx.set_shadow_blur(20.0);
-  ctx.set_line_join("bevel");
-  ctx.set_line_width(5.0);
+    ctx.set_shadow_color("#d53");
+    ctx.set_shadow_blur(20.0);
+    ctx.set_line_join("bevel");
+    ctx.set_line_width(5.0);
 }
